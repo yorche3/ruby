a6588ffc107c0c81adcf1872d8c1ed4f35dfa077
yorche3 <hyaoki123@gmail.com>
2026-09-19 18:20:48 -0600

    feat(algorithms): add naive sort implementation

--- core/algorithms/naive_sort/src/naive_sort.rb
+++ core/algorithms/naive_sort/src/naive_sort.rb
@@ -13,3 +13,53 @@
 #
 # Implementación pendiente: la escribe el autor. Esta delegación solo genera el
 # esqueleto y las pruebas unitarias.
+module NaiveSort
+  def self.selection_sort(arr)
+    return nil if arr.nil?
+    n = arr.length
+    return arr if n < 2
+    (0...n-1).each do |i|
+      min_index = i
+      (i+1...n).each do |j|
+        min_index = j if arr[j] < arr[min_index]
+      end
+      if min_index != i
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+      end
+    end
+    arr
+  end
+
+  def self.bubble_sort(arr)
+    return nil if arr.nil?
+    n = arr.length
+    return arr if n < 2
+    (0...n-1).each do |i|
+      swapped = false
+      (0...n-i-1).each do |j|
+        if arr[j] > arr[j+1]
+          arr[j], arr[j+1] = arr[j+1], arr[j]
+          swapped = true
+        end
+      end
+      break unless swapped
+    end
+    arr
+  end
+
+  def self.insertion_sort(arr)
+    return nil if arr.nil?
+    n = arr.length
+    return arr if n < 2
+    (1...n).each do |i|
+      key = arr[i]
+      j = i - 1
+      while j >= 0 && arr[j] > key
+        arr[j + 1] = arr[j]
+        j -= 1
+      end
+      arr[j + 1] = key
+    end
+    arr
+  end
+end
